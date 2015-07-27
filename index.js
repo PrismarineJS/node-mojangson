86f73fee6ddd19b417b25d1d60605cf7b1ee9db1
Romain Beaumont <romain.rom1@gmail.com>
2015-07-27 18:45:53 +0200

    display the whole unparsed text in case of error add a simple example file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var parser = require("./grammar").parser;
 
-module.exports={parse:function(text){return parser.parse(text)}};
+module.exports={parse:function(text){
+  try {
+    return parser.parse(text);
+  }
+  catch(e) {
+    console.log("Error parsing text '"+text+"'");
+    throw e;
+  }
+}};
