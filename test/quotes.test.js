b58b50ac7fc62500a81e723a7a95be741ee0c9de
Foresteam <heler1141@gmail.com>
2022-05-17 00:02:48 +0300

    Force quotes tests

---
+++ test/quotes.test.js
@@ -0,0 +1,18 @@
+/* eslint-env mocha */
+const assert = require('assert')
+const mojangson = require('../')
+
+describe('Always use quotes for names and strings', () => {
+  // Seems that from a certain MC version SNBTs stopped working without quotes (as for 1.18.2)
+  [
+    '{"var":"value1","var1":123b,"var2":"value3","var3":"value4"}',
+    '[B;1b,2b,3b]',
+    '{"id":"minecraft:yellow_shulker_box","Count":1b,"tag":{"BlockEntityTag":{"CustomName":"Stacked Totems","x":0,"y":0,"z":0,"id":"minecraft:shulker_box","Lock":""},"display":{"Name":"Stacked Totems"}},"Damage":0s}',
+    '{"SomeField":"кириллиц А"}',
+    '{"AField":"\\""}'
+  ].forEach(s => {
+    it('sould be unchanged', () => {
+      assert.strictEqual(mojangson.stringify(mojangson.parse(s), true), s)
+    })
+  })
+})
